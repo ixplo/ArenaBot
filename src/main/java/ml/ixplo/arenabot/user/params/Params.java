46cc5258913e885c14a76a9a47fb12318c795653
ixplo <ixplo@ya.ru>
2018-03-03 10:17:36 +0300

    sonar

--- src/main/java/ml/ixplo/arenabot/user/params/Params.java
+++ src/main/java/ml/ixplo/arenabot/user/params/Params.java
@@ -4,10 +4,10 @@ import java.lang.reflect.Field;
 import java.util.List;
 
 public class Params {
-    private List<Field> params;
+    private List<Field> fields;
 
-    public List<Field> getParams() {
-        return params;
+    public List<Field> get() {
+        return fields;
     }
 
     @Override
@@ -17,11 +17,11 @@ public class Params {
 
         Params params1 = (Params) o;
 
-        return params != null ? params.equals(params1.params) : params1.params == null;
+        return fields != null ? fields.equals(params1.fields) : params1.fields == null;
     }
 
     @Override
     public int hashCode() {
-        return params != null ? params.hashCode() : 0;
+        return fields != null ? fields.hashCode() : 0;
     }
 }
