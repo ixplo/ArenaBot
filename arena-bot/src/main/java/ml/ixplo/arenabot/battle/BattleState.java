bd37652bf283c5811cd2a1634c15ca0b9546ac18
ixplo <ixplo@ya.ru>
2018-04-09 00:52:38 +0300

    checkstyle

--- arena-bot/src/main/java/ml/ixplo/arenabot/battle/BattleState.java
+++ arena-bot/src/main/java/ml/ixplo/arenabot/battle/BattleState.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BattleState {
+    private static final int HASH_CONST = 31;
     private List<ArenaUser> members = new ArrayList<>();
     private List<Integer> curMembersId = new ArrayList<>();
     private List<Team> teams = new ArrayList<>();
@@ -44,24 +45,34 @@ public class BattleState {
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
 
-        BattleState that = (BattleState) o;
+        BattleState that = (BattleState) obj;
 
-        if (members != null ? !members.equals(that.members) : that.members != null) return false;
-        if (curMembersId != null ? !curMembersId.equals(that.curMembersId) : that.curMembersId != null) return false;
-        if (teams != null ? !teams.equals(that.teams) : that.teams != null) return false;
+        if (members != null ? !members.equals(that.members) : that.members != null) {
+            return false;
+        }
+        if (curMembersId != null ? !curMembersId.equals(that.curMembersId) : that.curMembersId != null) {
+            return false;
+        }
+        if (teams != null ? !teams.equals(that.teams) : that.teams != null) {
+            return false;
+        }
         return curTeamsId != null ? curTeamsId.equals(that.curTeamsId) : that.curTeamsId == null;
     }
 
     @Override
     public int hashCode() {
         int result = members != null ? members.hashCode() : 0;
-        result = 31 * result + (curMembersId != null ? curMembersId.hashCode() : 0);
-        result = 31 * result + (teams != null ? teams.hashCode() : 0);
-        result = 31 * result + (curTeamsId != null ? curTeamsId.hashCode() : 0);
+        result = HASH_CONST * result + (curMembersId != null ? curMembersId.hashCode() : 0);
+        result = HASH_CONST * result + (teams != null ? teams.hashCode() : 0);
+        result = HASH_CONST * result + (curTeamsId != null ? curTeamsId.hashCode() : 0);
         return result;
     }
 }
